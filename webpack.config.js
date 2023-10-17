ea9df165458eb3475554086ff9b9e035d3a5d046
bdxygy <budisantoso.devapp@gmail.com>
2023-10-17 09:21:04 +0700

    FEAT: Add gitignore

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { EsbuildPlugin } = require('esbuild-loader');
+
+module.exports = {
+    entry: './assets/index.js',
+    mode: 'production',
+    output: {
+        filename: 'main.min.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        // new MiniCssExtractPlugin({
+        //     filename: 'main.min.css',
+        // }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'esbuild-loader',
+                options: {
+                    target: 'es2015',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    // MiniCssExtractPlugin.loader,
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new EsbuildPlugin({
+                target: 'es2015',
+                css: true,
+            }),
+        ],
+    },
+};
